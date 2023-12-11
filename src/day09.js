52eb89f825b5d1b2cb2325637b55de7598c02cc6
David M. Lee <leedm777@yahoo.com>
2023-12-10 20:03:44 -0600

    Day 9, part 1

---
+++ src/day09.js
@@ -0,0 +1,41 @@
+import _ from "lodash";
+
+function parseSequence(line) {
+  return _(line)
+    .split(" ")
+    .map((s) => _.parseInt(s, 10))
+    .value();
+}
+
+function derivative(sequence) {
+  return _(_.take(sequence, _.size(sequence) - 1))
+    .zip(_.drop(sequence, 1))
+    .map(([a, b]) => b - a)
+    .value();
+}
+
+function nextValue(sequence) {
+  if (_.every(sequence, (n) => n === 0)) {
+    return 0;
+  }
+  const d = derivative(sequence);
+  const delta = nextValue(d);
+  return _.last(sequence) + delta;
+}
+
+/**
+ * @param {Array<string>} input Puzzle input
+ * @return {string} Puzzle output
+ */
+export function part1(input) {
+  const sequences = _.map(input, parseSequence);
+  return _(sequences).map(nextValue).sum();
+}
+
+/**
+ * @param {Array<string>} input Puzzle input
+ * @return {string} Puzzle output
+ */
+export function part2(input) {
+  return "TODO";
+}
---
+++ src/day09.spec.js
@@ -0,0 +1,29 @@
+import { part1, part2 } from "./day09";
+import { readInput } from "./aoc";
+
+const puzzleInput = readInput("./src/day09.txt");
+const exampleInput = ["0 3 6 9 12 15", "1 3 6 10 15 21", "10 13 16 21 30 45"];
+
+describe("day09", () => {
+  describe("part 1", () => {
+    it("should work with the sample", () => {
+      const actual = part1(exampleInput);
+      expect(actual).toStrictEqual(114);
+    });
+    it("should work with the puzzle input", () => {
+      const actual = part1(puzzleInput);
+      expect(actual).toStrictEqual(1789635132);
+    });
+  });
+
+  describe.skip("part 2", () => {
+    it("should work with the sample", () => {
+      const actual = part2(exampleInput);
+      expect(actual).toStrictEqual();
+    });
+    it("should work with the puzzle input", () => {
+      const actual = part2(puzzleInput);
+      expect(actual).toStrictEqual();
+    });
+  });
+});
