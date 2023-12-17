5908ef95b18d8e905a7858b4b032ec8ac009468a
David M. Lee <leedm777@yahoo.com>
2023-12-16 19:16:55 -0600

    Day 15, part 2

--- src/day15.js
+++ src/day15.js
@@ -2,6 +2,7 @@ import _ from "lodash";
 
 /**
  * @param {string} str String to hash
+ @return {number} Hash value
  */
 export function hash(str) {
   return _.reduce(
@@ -19,7 +20,7 @@ export function hash(str) {
 
 /**
  * @param {Array<string>} input Puzzle input
- * @return {string} Puzzle output
+ * @return {number} Puzzle output
  */
 export function part1([input]) {
   return _(input).split(",").map(hash).sum();
@@ -27,8 +28,41 @@ export function part1([input]) {
 
 /**
  * @param {Array<string>} input Puzzle input
- * @return {string} Puzzle output
+ * @return {number} Puzzle output
  */
 export function part2([input]) {
-  return "TODO";
+  const boxes = _.times(256, () => []);
+  const cmds = _.split(input, ",");
+  for (const cmd of cmds) {
+    if (_.endsWith(cmd, "-")) {
+      // Remove lens from box
+      const labelToRemove = cmd.slice(0, -1);
+      const idx = hash(labelToRemove);
+      boxes[idx] = _.filter(boxes[idx], ({ label }) => label !== labelToRemove);
+      continue;
+    }
+
+    // Possibly add lens
+    const [labelToAdd, focalLengthStr] = _.split(cmd, "=");
+    const focalLength = _.parseInt(focalLengthStr, 10);
+    const boxIdx = hash(labelToAdd);
+    const lens = _.find(boxes[boxIdx], ({ label }) => label === labelToAdd);
+    if (lens) {
+      // existing lens; replace it
+      lens.focalLength = focalLength;
+    } else {
+      boxes[boxIdx] = [...boxes[boxIdx], { label: labelToAdd, focalLength }];
+    }
+  }
+
+  return _(boxes)
+    .map((box, boxIdx) => {
+      return _(box)
+        .map(
+          ({ focalLength }, lensIdx) =>
+            (boxIdx + 1) * (lensIdx + 1) * focalLength,
+        )
+        .sum();
+    })
+    .sum();
 }
